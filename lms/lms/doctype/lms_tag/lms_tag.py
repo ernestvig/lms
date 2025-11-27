bec4060a00b830e3f81081813ea8e42891cfed3b
african-god <Owurakwaku165@gmail.com>
2025-11-27 15:16:41 +0000

    Refactor LMSTag module to enable retrieval of all tags with optional limit

--- lms/lms/doctype/lms_tag/lms_tag.py
+++ lms/lms/doctype/lms_tag/lms_tag.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LMSTag(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_all_tags(limit=None):
+    tags = frappe.get_all("LMSTag", fields=["name", "tag_name"], limit=limit)
+    return tags
